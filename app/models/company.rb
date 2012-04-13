a496efddbbfe5df721d4b13ee65cbd1fac4cbd8e
Ian Mooney <ian@yoomee.com>
2012-04-13 16:32:44 +0100

    Added links and contact_info to company

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,9 +2,30 @@ class Company < ActiveRecord::Base
   
   include YmCore::Model
   
+  image_accessor :image
+  
   has_many :posts, :as => :target
   
+  has_snippets :contact1_name, :contact1_email, :contact1_phone, :contact2_name, :contact2_email, :contact2_phone, :website_url, :facebook_url, :twitter_url, :youtube_url, :vimeo_url, :soundcloud_url
+  
   validates_presence_of :name
-  image_accessor :image
+  validates :contact1_email, :contact2_email, :email => true, :allow_blank => true
+  validates :website_url, :facebook_url, :twitter_url, :youtube_url, :vimeo_url, :soundcloud_url, :url => true, :allow_blank => true
+  validates_property :format, :of => :image, :in => [:jpeg, :jpg, :png, :gif], :message => "must be an image"
+  
+  def contact1_details
+    [contact1_name, contact1_email, contact1_phone].compact
+  end
+  
+  def contact2_details
+    [contact2_name, contact2_email, contact2_phone].compact
+  end
+  
+  def social_urls
+    %w{facebook twitter youtube vimeo soundcloud}.collect do |site_name|
+      url = send("#{site_name}_url")
+      [site_name.humanize, url] if url.present?
+    end.compact
+  end
   
 end
