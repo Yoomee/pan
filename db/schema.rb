4bafa8769be5ca06f5107512fb120689cae6258d
Ian Mooney <ian@yoomee.com>
2012-04-16 17:11:39 +0100

    Added user profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120413143946) do
+ActiveRecord::Schema.define(:version => 20120416155053) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(:version => 20120413143946) do
   create_table "users", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
+    t.text     "bio"
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
