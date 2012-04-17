8c50859f0f5f14098597177b068c5336c4c747b0
Si Wilkins <si@yoomee.com>
2012-04-17 16:04:26 +0100

    Added authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :authenticate
+
+  AUTH_USERS = { "pan" => "highlands123" }
+
+  private
+  def authenticate
+    return true unless Rails.env.production?
+    authenticate_or_request_with_http_basic do |username|
+      AUTH_USERS[username]
+    end
+  end
+  
 end
