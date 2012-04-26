256cb5ae7df2d82862291e6b3d376ddaa278fe10
Ian Mooney <ian@yoomee.com>
2012-04-26 15:50:21 +0100

    Added audiences to promoters

--- app/models/promoter.rb
+++ app/models/promoter.rb
@@ -2,7 +2,7 @@ class Promoter < ActiveRecord::Base
   
   include Organisation
 
-  acts_as_taggable_on :genres, :art_forms, :genre_interests, :art_form_interests, :funders
+  acts_as_taggable_on :genres, :art_forms, :genre_interests, :art_form_interests, :funders, :audiences
   
   has_many :users, :dependent => :nullify
   has_many :venues, :dependent => :destroy
