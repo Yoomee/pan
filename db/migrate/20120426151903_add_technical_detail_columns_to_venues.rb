e10073dc824dc9e5609f2f5e0d2923e5d26e8c52
Ian Mooney <ian@yoomee.com>
2012-04-26 17:35:59 +0100

    Technical fields on venue

---
+++ db/migrate/20120426151903_add_technical_detail_columns_to_venues.rb
@@ -0,0 +1,53 @@
+class AddTechnicalDetailColumnsToVenues < ActiveRecord::Migration
+
+  def change
+    change_table :venues do |t|
+      t.integer :size
+      t.integer :height
+      t.integer :num_seats_min
+      t.integer :num_seats_max
+      t.integer :num_seats_tiered
+      t.boolean :tiered_seating
+      t.integer :stage_depth
+      t.integer :stage_width
+      t.boolean :stage_curtaining
+      t.text :no_stage_text
+      t.boolean :full_blackout
+      t.boolean :curtains
+      t.boolean :high_windows
+      t.boolean :pa_system
+      t.boolean :stereo_sound
+      t.boolean :sound_technician
+      t.boolean :lighting
+      t.boolean :light_stand
+      t.boolean :light_rigging
+      t.boolean :light_washes
+      t.boolean :light_spots
+      t.boolean :light_dimmer
+      t.boolean :light_technician
+      t.text :light_installation_text
+      t.boolean :piano
+      t.boolean :piano_in_tune
+      t.text :piano_text
+      t.boolean :dressing_room
+      t.boolean :kitchen
+      t.boolean :bar
+      t.boolean :loop_system
+      t.boolean :wheelchair_accessible
+      t.integer :num_steps
+      t.integer :door_width
+      t.text :accessibility_text
+      t.boolean :help_for_entering
+      t.boolean :help_for_exiting
+      t.boolean :lorry_parking
+      t.boolean :prs_licence
+      t.boolean :alcohol_licence
+      t.boolean :theatre_licence
+      t.boolean :temporary_licence
+      t.boolean :outdoor_licence
+      t.boolean :public_entertainment_licence
+      t.text :licence_text
+    end
+  end
+  
+end
