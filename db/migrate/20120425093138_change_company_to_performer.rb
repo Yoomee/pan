17db7298058400fbdf197ab409d211fe19efe8d7
Ian Mooney <ian@yoomee.com>
2012-04-26 10:00:31 +0100

    Update tagging + posts with 'Company' to 'Performer'

--- db/migrate/20120425093138_change_company_to_performer.rb
+++ db/migrate/20120425093138_change_company_to_performer.rb
@@ -1,6 +1,17 @@
 class ChangeCompanyToPerformer < ActiveRecord::Migration
-  def change
+  
+  def self.up
     rename_table :companies, :performers
     rename_column :tours, :company_id, :performer_id
+    Post.where("target_type = 'Company'").update_all("target_type = 'Performer'")
+    ActsAsTaggableOn::Tagging.where("taggable_type = 'Company'").update_all("taggable_type = 'Performer'")
   end
+
+  def self.down
+    ActsAsTaggableOn::Tagging.where("taggable_type = 'Performer'").update_all("taggable_type = 'Company'")    
+    Post.where("target_type = 'Performer'").update_all("target_type = 'Company'")
+    rename_column :tours, :performer_id, :company_id    
+    rename_table :performers, :companies    
+  end
+  
 end
