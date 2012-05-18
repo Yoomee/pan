655645bf3e5393ed0328cfaf2a10c70c08a06bb0
Ian Mooney <ian@yoomee.com>
2012-05-18 17:21:53 +0100

    Added activity_items

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,9 @@ class User < ActiveRecord::Base
   has_many :links, :as => :attachable, :dependent => :destroy
   accepts_nested_attributes_for :links, :reject_if => :all_blank, :allow_destroy => true  
   
-  acts_as_taggable_on :skills
-  acts_as_taggable_on :skills_offered
-  acts_as_taggable_on :skills_needed
+  acts_as_taggable_on :skills, :skills_offered, :skills_needed
+  
+  after_create :record_activity
   
   def self.present_directory_letters
     order(:last_name).select("UCASE(SUBSTR(last_name,1,1)) AS letter").group("SUBSTR(last_name,1,1)").collect(&:letter)
@@ -24,6 +24,11 @@ class User < ActiveRecord::Base
   
   def description
     bio
+  end
+  
+  private
+  def record_activity
+    record_activity!(self)
   end
   
 end
