93b3dfc12c7ec4ecd9be5e2fb515cda8cc5858b6
Ian Mooney <ian@yoomee.com>
2012-06-06 17:09:54 +0100

    More insurances on venue, neatened technical details tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,9 @@
 module ApplicationHelper
   
   def description_tag(title, content)
+    if %w{yes no}.include?(content.downcase)
+      content = content_tag(:span, content, :class => "label #{content.downcase=='yes' ? 'label-success' : 'label-important'}")
+    end
     content_tag(:dt, title) + content_tag(:dd, content)
   end
   
