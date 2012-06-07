be9ff8ae2d4d83131daceb79e92d39c6ee3aee8d
Ian Mooney <ian@yoomee.com>
2012-06-07 17:10:10 +0100

    Fix for description_tag method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   
   def description_tag(title, content)
-    if %w{yes no}.include?(content.downcase)
-      content = content_tag(:span, content, :class => "label #{content.downcase=='yes' ? 'label-success' : 'label-important'}")
+    if %w{yes no}.include?(content.to_s.downcase)
+      content = content_tag(:span, content, :class => "label #{content.to_s.downcase=='yes' ? 'label-success' : 'label-important'}")
     end
     content_tag(:dt, title) + content_tag(:dd, content)
   end
