c3aec9ec53338d624127c07be31566734d601218
Ian Mooney <ian@yoomee.com>
2012-06-12 12:55:34 +0100

    Deleting users

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Pan::Application.routes.draw do
   
   root :to => 'home#index'
 
-  resources :performers
-  resources :performers, :only => [] do
+  resources :performers, :except => [:show]
+  resources :performers, :only => [:show] do
     resources :tours, :only => [:new, :index]    
     collection do
       get 'directory(/:letter)', :action => 'directory', :as => 'directory'
@@ -22,8 +22,8 @@ Pan::Application.routes.draw do
     end
   end
   
-  resources :promoters
-  resources :promoters, :only => [] do
+  resources :promoters, :except => [:show]
+  resources :promoters, :only => [:show] do
     resources :users, :only => [:new, :create]
     resources :venues
     collection do
