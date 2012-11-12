966c0fb04d56ee1e4f3fcf1d17606cbe558d49c9
Edward Andrews <edward@yoomee.com>
2012-11-12 15:00:03 +0000

    Stop exceptions in dev

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,4 +1,6 @@
-Pan::Application.config.middleware.use ExceptionNotifier,
-  :email_prefix => "[PAN] ",
-  :sender_address => "notifier <notifier@pan.yoomee.com>",
-  :exception_recipients => %w{developers@yoomee.com}
+if Rails.env.production?
+  Pan::Application.config.middleware.use ExceptionNotifier,
+    :email_prefix => "[PAN] ",
+    :sender_address => "notifier <notifier@pan.yoomee.com>",
+    :exception_recipients => %w{developers@yoomee.com}
+end
