6239db8126367ac0b9ec32e1f9efff104be6d779
Edward Andrews <edward@yoomee.com>
2012-11-12 14:59:34 +0000

    Generalize filter_helper

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -1,14 +1,16 @@
 module FilterHelper
 
-  def tag_link(label, param_tags, start_date, end_date, classes, add_remove=false)
+  def tag_link(label, param_options, classes, add_remove=false)
     
+    label.gsub!(/-/, ' ')
     label = label + "&nbsp;" + content_tag(:i, "", :class => "icon-remove") if add_remove
+    
     if current_page?(shows_path)
-      link_to(shows_path(:tags => param_tags, :start_date => start_date, :end_date => end_date), :class => classes) do
+      link_to(shows_path(param_options), :class => classes) do
         content_tag :span, label.html_safe, :class => "label tag-label"
       end
     elsif current_page?(diary_path)
-      link_to(diary_path(:tags => param_tags, :start_date => start_date, :end_date => end_date), :class => classes) do
+      link_to(diary_path(param_options), :class => classes) do
         content_tag :span, label.html_safe, :class => "label tag-label"
       end
     end
