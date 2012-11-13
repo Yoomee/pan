26649aadb61e70c2793778e792cdc8bb94114aa3
Edward Andrews <edward@yoomee.com>
2012-11-13 10:00:58 +0000

    Add fontdeck stylesheet

--- config/application.rb
+++ config/application.rb
@@ -44,6 +44,9 @@ module Pan
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Don't require production db to precompile assets
+    config.assets.initialize_on_precompile = false
   end
 end
 
