768b8b46e6997fde80d15643664e91465fa30fc5
Edward Andrews <edward@yoomee.com>
2012-11-19 15:41:12 +0000

    Add venues to diary

--- app/controllers/diary_controller.rb
+++ app/controllers/diary_controller.rb
@@ -8,11 +8,9 @@ class DiaryController < ApplicationController
     @end_date = params[:end_date]
     @tour_id = params[:tour_id]
     @organisation_id = params[:organisation_id]
+    @venue_id = params[:venue_id]
     
-    if @tour_id.present?
-      @month = params[:month]
-      @year = params[:year]
-    elsif @organisation_id.present?
+    if @tour_id.present? || @organisation_id.present? || @venue_id.present?
       @month = params[:month]
       @year = params[:year]
     else
@@ -42,6 +40,9 @@ class DiaryController < ApplicationController
     end
     if @organisation_id.present?
       @dates = @dates.where(:venue_id => Promoter.find(@organisation_id).venues)
+    end
+    if @venue_id.present?
+      @dates = @dates.where(:venue_id => @venue_id)
     end
 
   end
--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -2,16 +2,19 @@ module FilterHelper
 
   def tag_link(label, param_options, classes, add_remove=false)
     
-    if (param_options[:tour_id].present? || param_options[:organisation_id].present?) && (param_options[:start_date].present? || param_options[:end_date].present?)
+    if (param_options[:tour_id].present? || param_options[:organisation_id].present? || param_options[:venue_id].present?) && (param_options[:start_date].present? || param_options[:end_date].present?)
       param_options[:month] = param_options[:start_date].split("/")[1]
       param_options[:year] = param_options[:start_date].split("/")[2]
       param_options[:tour_id] = ""
       param_options[:organisation_id] = ""
+      param_options[:venue_id] = ""
     end  
     if param_options[:month].blank?
       param_options[:month] = Date.today.month
       param_options[:year] = Date.today.year
     end
+    
+    param_options.delete_if {|k, v| v.blank?}
 
     label.gsub!(/-/, ' ')
     label = label + "&nbsp;" + content_tag(:i, "", :class => "icon-remove") if add_remove
