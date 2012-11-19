28f6e1dda55ee6a5a98d7e0c44eb96a96770036e
Edward Andrews <edward@yoomee.com>
2012-11-19 13:27:20 +0000

    A diary for promoters - as organisations

--- app/controllers/diary_controller.rb
+++ app/controllers/diary_controller.rb
@@ -7,8 +7,12 @@ class DiaryController < ApplicationController
     @start_date = params[:start_date]
     @end_date = params[:end_date]
     @tour_id = params[:tour_id]
+    @organisation_id = params[:organisation_id]
     
     if @tour_id.present?
+      @month = params[:month]
+      @year = params[:year]
+    elsif @organisation_id.present?
       @month = params[:month]
       @year = params[:year]
     else
@@ -18,7 +22,7 @@ class DiaryController < ApplicationController
 
     @all_tags = Tour.genre_counts.order(:name)
 
-    @dates =  TourDate.where(:booked => true)
+    @dates =  TourDate.where(:booked => true).order(:date)
 
     if @month.present? && @year.present?
       @dates = @dates.where('MONTH(date) = :month AND YEAR(date) = :year', :month => @month, :year => @year)
@@ -35,6 +39,9 @@ class DiaryController < ApplicationController
     end
     if @tour_id.present?
       @dates = @dates.where(:tour_id => @tour_id)
+    end
+    if @organisation_id.present?
+      @dates = @dates.where(:venue_id => Promoter.find(@organisation_id).venues)
     end
 
   end
--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -2,6 +2,12 @@ module FilterHelper
 
   def tag_link(label, param_options, classes, add_remove=false)
     
+    if (param_options[:tour_id].present? || param_options[:organisation_id].present?) && (param_options[:start_date].present? || param_options[:end_date].present?)
+      param_options[:month] = param_options[:start_date].split("/")[1]
+      param_options[:year] = param_options[:start_date].split("/")[2]
+      param_options[:tour_id] = ""
+      param_options[:organisation_id] = ""
+    end  
     if param_options[:month].blank?
       param_options[:month] = Date.today.month
       param_options[:year] = Date.today.year
