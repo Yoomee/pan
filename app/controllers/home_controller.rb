283d125ed6a834cf9673ce0cd84ec2369575645e
Andy Mayer <andy@yoomee.com>
2012-11-25 12:52:38 +0000

    Add noticeboard section

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
   def index
     if current_user
+      @pages = Page.find_by_slug(:noticeboard).children
       render :index
     else
       redirect_to sign_in_path
