4baaf9081a62a2508feb8bbea88a9094839ad7e5
Edward Andrews-Hodgson <edward@yoomee.com>
2013-07-08 12:02:31 +0100

    Refactor

--- app/controllers/directory_controller.rb
+++ app/controllers/directory_controller.rb
@@ -3,16 +3,18 @@ class DirectoryController < ApplicationController
   before_filter :set_up_queries_and_classes
   
   def directory
-    if @search_classes.empty? && @conditions.present?
-      @everything = []
+    if @query.present? && @conditions.present?
+      @everything = ThinkingSphinx.search(@query, :conditions => @conditions, :classes => @search_classes, :match_mode => :extended, :order => :name_sort, :sort_mode => :asc, :per_page => 40)
+    elsif @query.present?
+      @everything = ThinkingSphinx.search(@query, :match_mode => :extended, :classes => @search_classes, :order => :name_sort, :sort_mode => :asc, :per_page => 40)
+    elsif @conditions.present?
+      # If we had a newer version of sphinx we could do this for first letter of first word
+      # @everything = ThinkingSphinx.search("@name[1] ^?", @letter, :conditions => @conditions, :classes => @search_classes, :match_mode => :extended, :order => :name_sort, :sort_mode => :asc, :per_page => 40)
+      @everything = ThinkingSphinx.search(:conditions => @conditions, :classes => @search_classes, :match_mode => :extended, :order => :name_sort, :sort_mode => :asc, :per_page => 40)
+    elsif @search_classes.present?
+      @everything = ThinkingSphinx.search(:classes => @search_classes, :match_mode => :extended, :order => :name_sort, :sort_mode => :asc, :per_page => 40)
     else
-      if @conditions.present?
-        # If we had a newer version of sphinx we could do this for first letter of first word
-        # @everything = ThinkingSphinx.search("@name[1] ^?", @letter, :conditions => @conditions, :classes => @search_classes, :match_mode => :extended, :order => :name_sort, :sort_mode => :asc, :per_page => 200)
-        @everything = ThinkingSphinx.search(:conditions => @conditions, :classes => @search_classes, :match_mode => :extended, :order => :name_sort, :sort_mode => :asc, :per_page => 200)
-      else
-        @everything = ThinkingSphinx.search(:classes => @search_classes, :match_mode => :extended, :order => :name_sort, :sort_mode => :asc, :per_page => 200)
-      end
+      @everything = []
     end
     render :template => 'organisations/directory'
   end
@@ -22,9 +24,9 @@ class DirectoryController < ApplicationController
     
     if @query.present?
       if @conditions.present?
-        @everything = ThinkingSphinx.search(@query, :conditions => @conditions, :classes => @search_classes, :match_mode => :extended, :order => :name_sort, :sort_mode => :asc, :per_page => 200)
+        @everything = ThinkingSphinx.search(@query, :conditions => @conditions, :classes => @search_classes, :match_mode => :extended, :order => :name_sort, :sort_mode => :asc, :per_page => 40)
       else
-        @everything = ThinkingSphinx.search(@query, :match_mode => :extended, :classes => @search_classes, :order => :name_sort, :sort_mode => :asc, :per_page => 200)
+        @everything = ThinkingSphinx.search(@query, :match_mode => :extended, :classes => @search_classes, :order => :name_sort, :sort_mode => :asc, :per_page => 40)
       end
     else
       @everything = []
@@ -39,6 +41,7 @@ class DirectoryController < ApplicationController
     @region = params[:region]
     @tags = [*params[:tags]]
     @type = params[:type]
+    @query = strip_tags(params[:q]).to_s.strip
 
     @search_classes = [Performer, Promoter, Tour, Venue, User]
     @search_classes &= [Tour] if @collection.present?
