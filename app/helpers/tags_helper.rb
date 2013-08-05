d9731a729b5713747895342435b1d28bf948b350
Chrystal Griffiths <chrystal@yoomee.com>
2013-08-05 12:54:45 +0100

    Put skills tags back in as static text

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -3,7 +3,12 @@ module TagsHelper
   def comma_list_tags(resource, tag_context)
     out = []
     resource.send("#{tag_context.to_s.singularize}_list").each do |tag_name|
-      out << link_to_tag_in_context(tag_name, resource.class.to_s.underscore.pluralize, tag_context)
+      # out << tag_context == :skills ? tag_name.to_s.titleize.humanize : link_to_tag_in_context(tag_name, resource.class.to_s.underscore.pluralize, tag_context)
+      if tag_context == :skills
+        out << tag_name.to_s.titleize.humanize
+      else
+        out << link_to_tag_in_context(tag_name, resource.class.to_s.underscore.pluralize, tag_context)
+      end
     end
     out.join(', ').html_safe
   end
@@ -18,6 +23,5 @@ module TagsHelper
 
   def link_to_tag_in_context(tag_name, resource_controller, tag_context, options = {})
     link_to(tag_name.to_s.titleize.humanize, send("tag_#{resource_controller}_path", tag_context.to_s, tag_name), options)
-  end
-  
+  end  
 end
