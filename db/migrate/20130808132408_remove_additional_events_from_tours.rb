edfeea4f27b20204ab0668e6c8ecd8c9a4614a6e
Chrystal Griffiths <chrystal@yoomee.com>
2013-08-13 17:07:46 +0100

    Remove unecessary migration

--- db/migrate/20130808132408_remove_additional_events_from_tours.rb
+++
@@ -1,9 +0,0 @@
-class RemoveAdditionalEventsFromTours < ActiveRecord::Migration
-  def up
-    remove_column :tours, :additional_events
-  end
-
-  def down
-    add_column :tours, :additional_events, :string
-  end
-end
