0f981e64b335c74a6dc5f8da9b043038a538c7ac
Chrystal Griffiths <chrystal@yoomee.com>
2013-09-10 15:27:22 +0100

    New performer home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,6 +10,7 @@ class HomeController < ApplicationController
       @latest_shows = Tour.order(:created_at).reverse_order.limit(5)
       @upcoming_events = TourDate.in_pan_venue.future.order(:date).limit(3)
       @recent_activity = YmActivity::ActivityItem.paginate(:per_page => 3, :page => params[:page])
+      @tours = Tour.where("performer_id = #{current_user.performer.id}") if current_user.performer
       render :index
     else
       redirect_to sign_in_path
