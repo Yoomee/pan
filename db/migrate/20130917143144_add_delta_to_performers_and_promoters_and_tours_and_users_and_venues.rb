ffbe07ff8862ffff04c69fb2a770453e513c02d3
Chrystal Griffiths <chrystal@yoomee.com>
2013-09-17 17:59:39 +0100

    Uncomment migration

--- db/migrate/20130917143144_add_delta_to_performers_and_promoters_and_tours_and_users_and_venues.rb
+++ db/migrate/20130917143144_add_delta_to_performers_and_promoters_and_tours_and_users_and_venues.rb
@@ -1,10 +1,10 @@
 class AddDeltaToPerformersAndPromotersAndToursAndUsersAndVenues < ActiveRecord::Migration
   def change
-    # add_column :performers, :delta, :boolean, :default => true
-    # add_column :promoters, :delta, :boolean, :default => true
-    # add_column :tours, :delta, :boolean, :default => true
-    # add_column :users, :delta, :boolean, :default => true
-    # add_column :venues, :delta, :boolean, :default => true
+    add_column :performers, :delta, :boolean, :default => true
+    add_column :promoters, :delta, :boolean, :default => true
+    add_column :tours, :delta, :boolean, :default => true
+    add_column :users, :delta, :boolean, :default => true
+    add_column :venues, :delta, :boolean, :default => true
     add_column :groups, :delta, :boolean, :default => true
     add_column :resources, :delta, :boolean, :default => true
   end
