d17bd33a5068bfb749aa1e186b785e63953132c4
Andy Mayer <andy@yoomee.com>
2014-09-24 18:15:56 +0100

    Fix bug if name is blank

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -1,27 +1,27 @@
 class Performer < ActiveRecord::Base
-  
+
   include YmLikes::Likeable
   include YmActivity::Recordable
   include Organisation
   include HasLinks
   include HasReviews
-  
+
   define_index do
     indexes name, :sortable => true
     indexes description
     indexes genre_tags(:name), :as => :genres
     has created_at, updated_at
-    set_property :delta => true 
+    set_property :delta => true
   end
-  
+
   image_accessor :image
   acts_as_taggable_on :genres, :art_forms, :funders, :work_scales, :categories, :tags
   has_many :genre_tags, :through => :taggings, :source => :tag, :class_name => "ActsAsTaggableOn::Tag",
           :conditions => "taggings.context = 'genres'"
 
   validates :description, length: {maximum: 1000}
-  
-  
+
+
   has_many :posts, :as => :target, :conditions => {:is_update => false}
   has_many :updates, :class_name => "Post", :as => :target, :conditions => {:is_update => true}
   has_many :tours, :dependent => :destroy
@@ -30,7 +30,7 @@ class Performer < ActiveRecord::Base
 
   has_many :performer_users, :dependent => :destroy
   has_many :users, :through => :performer_users
-  
+
   has_many :notifications, :as => :resource, :dependent => :destroy
 
   has_many :external_reviews, :as => :reviewable, :dependent => :destroy
@@ -38,10 +38,10 @@ class Performer < ActiveRecord::Base
   accepts_nested_attributes_for :users
 
   has_snippets :contact1_name, :contact1_email, :contact1_phone, :contact2_name, :contact2_email, :contact2_phone
-  
+
   validates :contact1_email, :contact2_email, :email => true, :allow_blank => true
   validate :validate_user, :if => :new_record?
-  
+
   after_update :create_notifications_if_favourite
 
   boolean_accessor :create_user_from_performer
@@ -66,32 +66,32 @@ class Performer < ActiveRecord::Base
   end
 
   def split_name(name)
-    name = name.split(' ')     
+    name = name.blank? ? [""] : name.split(' ')     
     name.push('n/a') if name.count == 1
     name
   end
-  
+
   def contact1_details
     [contact1_name, contact1_email, contact1_phone].compact
   end
-  
+
   def contact2_details
     [contact2_name, contact2_email, contact2_phone].compact
   end
-  
+
   def current_tour
     tours.order(:end_on).where("end_on > :now", now: Time.now).first
-  end  
-  
+  end
+
   def future_tours
     tours.order(:end_on).where("end_on > :now", now: Time.now).drop(1)
   end
-  
+
   private
   def create_notifications_if_favourite
     likers.each do |liker|
       notifications.create(:user => liker, :context => "favourite_updated")
     end
   end
-  
+
 end
